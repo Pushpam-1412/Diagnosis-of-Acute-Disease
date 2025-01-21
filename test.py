dbec8b80e7fa302013fd25fb63c3a9b5c0443866
Abdul Rahman Mansoori <rajuchotte@gmail.com>
2025-01-21 12:51:40 +0530

    Removed

--- test.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.cuda.is_available())  # Should be False on macOS without GPU
-print(torch.backends.mps.is_available())  # Should be True on Apple Silicon
